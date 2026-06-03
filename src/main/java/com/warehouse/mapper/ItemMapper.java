b5efd3bd91368f9427da41310a10c2deafc0507b
Aleksandr Sovetnikov <ALEX-6-3-7@yandex.ru>
2026-06-03 21:21:00 +0300

    feat: добавлено редактирование товара-исправленный

--- src/main/java/com/warehouse/mapper/ItemMapper.java
+++ src/main/java/com/warehouse/mapper/ItemMapper.java
@@ -3,15 +3,20 @@ package com.warehouse.mapper;
 import com.warehouse.dto.request.CreateItemRequest;
 import com.warehouse.dto.responce.ItemResponse;
 import com.warehouse.dto.ItemDetails;
+import com.warehouse.dto.ItemUpdateRequest;
 import com.warehouse.entity.Item;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface ItemMapper {
     Item toEntity(CreateItemRequest request);
     ItemResponse toResponse(Item item);
 
-    @Mapping(target = "currentStock", ignore = true) // игнорируем, если нет в Item
+    @Mapping(target = "currentStock", ignore = true)
+        // игнорируем, если нет в Item
     ItemDetails toItemDetails(Item item);
+
+    Item updateItemFromRequest(ItemUpdateRequest request, @MappingTarget Item item);
 }
--- src/main/java/com/warehouse/service/ItemService.java
+++ src/main/java/com/warehouse/service/ItemService.java
@@ -6,3 +6,17 @@ import com.warehouse.dto.responce.ItemResponse;
 public interface ItemService {
     ItemResponse createItem(CreateItemRequest request);
 }
+
+        item = itemMapper.updateItemFromRequest(request, item);
+
+        Item savedItem = itemRepository.save(item);
+        ItemDetails details = itemMapper.toItemDetails(savedItem);
+
+        stockRepository.findByItemId(savedItem.getId())
+                .ifPresent(stock -> {
+                    details.setCurrentStock(stock.getQuantity());
+                });
+        if (details.getCurrentStock() == null) details.setCurrentStock(0);
+        return details;
+    }
+}
