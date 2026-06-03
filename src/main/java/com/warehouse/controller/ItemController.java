ba55fa84d090570a9c6357244afde00048eb9123
Aleksandr Sovetnikov <ALEX-6-3-7@yandex.ru>
2026-06-03 05:23:49 +0300

    feat: добавлено редактирование товара

--- src/main/java/com/warehouse/controller/ItemController.java
+++ src/main/java/com/warehouse/controller/ItemController.java
@@ -2,10 +2,13 @@ package com.warehouse.controller;
 
 import com.warehouse.dto.request.CreateItemRequest;
 import com.warehouse.dto.responce.ItemResponse;
+import com.warehouse.dto.ItemDetails;
+import com.warehouse.dto.ItemUpdateRequest;
 import com.warehouse.service.ItemService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,5 +24,13 @@ public class ItemController {
     @PreAuthorize("hasRole('ADMIN')")
     public ItemResponse createItem(@Valid @RequestBody CreateItemRequest request) {
         return itemService.createItem(request);
+    @PutMapping("/{itemId}")
+    @PreAuthorize("hasRole('ADMIN')") // Spring подставит ROLE_ и найдет ROLE_ADMIN
+    public ResponseEntity<ItemDetails> updateItem(
+            @PathVariable Long itemId,
+            @Valid @RequestBody ItemUpdateRequest request) {
+
+        ItemDetails updatedItem = itemService.updateItem(itemId, request);
+        return ResponseEntity.ok(updatedItem);
     }
 }
--- src/main/java/com/warehouse/mapper/ItemMapper.java
+++ src/main/java/com/warehouse/mapper/ItemMapper.java
@@ -2,11 +2,16 @@ package com.warehouse.mapper;
 
 import com.warehouse.dto.request.CreateItemRequest;
 import com.warehouse.dto.responce.ItemResponse;
+import com.warehouse.dto.ItemDetails;
 import com.warehouse.entity.Item;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ItemMapper {
     Item toEntity(CreateItemRequest request);
     ItemResponse toResponse(Item item);
+
+    @Mapping(target = "currentStock", ignore = true) // игнорируем, если нет в Item
+    ItemDetails toItemDetails(Item item);
 }
