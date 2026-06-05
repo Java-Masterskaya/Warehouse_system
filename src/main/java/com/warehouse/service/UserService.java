7c0c9a7f4ca244de06181cd435dbb3135bd13082
Daniil Sebiakin <sebyackindaniil@ya.ru>
2026-06-05 20:25:47 +0300

    review fix

--- src/main/java/com/warehouse/service/UserService.java
+++ src/main/java/com/warehouse/service/UserService.java
@@ -2,37 +2,9 @@ package com.warehouse.service;
 
 import com.warehouse.dto.requests.UserCreateRequest;
 import com.warehouse.dto.responses.UserResponse;
-import com.warehouse.entity.User;
-import com.warehouse.exception.DuplicateUsernameException;
-import com.warehouse.mapper.UserMapper;
-import com.warehouse.repository.UserRepository;
 import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
 
-@Service
-@RequiredArgsConstructor
-@Slf4j
-public class UserService {
-    private final UserRepository userRepository;
-    private final UserMapper userMapper;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-
+public interface UserService {
     @Transactional
-    public UserResponse createUser(UserCreateRequest request) {
-        log.debug("Create user with name '{}'", request.getUsername());
-        if (userRepository.existsByUsername(request.getUsername())) {
-            log.warn("User '{}' is already exist", request.getUsername());
-            throw new DuplicateUsernameException(request.getUsername());
-        }
-
-        User user = userMapper.toEntity(request);
-        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        userRepository.save(user);
-
-        log.info("User created: name={}", user.getUsername());
-        return userMapper.toResponse(user);
-    }
+    UserResponse createUser(UserCreateRequest request);
 }
