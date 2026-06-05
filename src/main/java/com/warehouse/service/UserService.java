dc82304eb7303f751a364d9c2f620e16daa65f87
Daniil Sebiakin <sebyackindaniil@ya.ru>
2026-06-05 22:49:51 +0300

    checkstyle fix

--- src/main/java/com/warehouse/service/UserService.java
+++ src/main/java/com/warehouse/service/UserService.java
@@ -2,9 +2,7 @@ package com.warehouse.service;
 
 import com.warehouse.dto.requests.UserCreateRequest;
 import com.warehouse.dto.responses.UserResponse;
-import jakarta.transaction.Transactional;
 
 public interface UserService {
-    @Transactional
     UserResponse createUser(UserCreateRequest request);
 }
