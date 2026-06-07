464209eca164752bebba391e6780e8c35bed73a5
Andrey Potokin <potokin.and@yandex.ru>
2026-06-07 19:36:19 +0300

    refactor: удалить неиспользуемые файлы ресурсов тестов

--- src/test/resources/cleanup.sql
+++
@@ -1,6 +0,0 @@
--- cleanup.sql
--- Delete test data
-DELETE FROM stock_movements WHERE item_id = 1;
-DELETE FROM stock WHERE item_id = 1;
-DELETE FROM items WHERE id = 1;
-DELETE FROM users WHERE id = 1;
