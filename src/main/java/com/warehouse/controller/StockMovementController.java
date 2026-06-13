1b54425f6a9bcf4cc5c9b840d96baf0c13aa8c0f
Daniil Sebiakin <sebyackindaniil@ya.ru>
2026-06-13 23:34:11 +0300

    fix

--- src/main/java/com/warehouse/controller/StockMovementController.java
+++ src/main/java/com/warehouse/controller/StockMovementController.java
@@ -46,7 +46,8 @@ public class StockMovementController {
     public StockMovementResponse registerReceipt(
         @Valid @RequestBody CreateStockMovementRequest request,
         @AuthenticationPrincipal User currentUser) {
-        log.debug("Received stock movement register request: itemId={}, quantity={}", request.itemId(), request.quantity());
+        log.debug("Received stock movement register request: itemId={}, quantity={}", request.itemId(),
+                request.quantity());
         return stockMovementService.registerReceipt(request, currentUser);
     }
 
@@ -55,7 +56,8 @@ public class StockMovementController {
     @PreAuthorize("hasRole('ADMIN')")
     public StockMovementResponse wrireOffReceipt(@Valid @RequestBody WriteOffStockMovementRequest request,
                                                  @AuthenticationPrincipal User currentUser) {
-        log.debug("Received stock movement writeOff request: itemId={}, quantity={}", request.itemId(), request.quantity());
+        log.debug("Received stock movement writeOff request: itemId={}, quantity={}", request.itemId(),
+                request.quantity());
         return stockMovementService.writeOffReceipt(request, currentUser);
     }
 }
