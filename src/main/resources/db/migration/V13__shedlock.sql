f2094754a6f0b295f1a139e5647994cd5a6c947b
Daria Leo <dashunkaleo@yandex.ru>
2026-07-18 10:23:33 +0300

    feat: migration for shedlock

---
+++ src/main/resources/db/migration/V13__shedlock.sql
@@ -0,0 +1,8 @@
+CREATE TABLE shedlock
+(
+    name       varchar(64)  NOT NULL,
+    lock_until timestamp    NOT NULL,
+    locked_at  timestamp    NOT NULL,
+    locked_by  varchar(255) NOT NULL,
+    PRIMARY KEY (name)
+);
