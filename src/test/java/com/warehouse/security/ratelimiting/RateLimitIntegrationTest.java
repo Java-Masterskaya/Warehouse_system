321f3d66f00b39c71c52bd2ebf33e116c4022316
Aleksandr Tsvyndra <aleks.tsvindra@gmail.com>
2026-07-25 22:33:48 +0300

    feature(REL-5): Add annotation SpringBootTest on the class RateLimitIntegratonTest

--- src/test/java/com/warehouse/security/ratelimiting/RateLimitIntegrationTest.java
+++ src/test/java/com/warehouse/security/ratelimiting/RateLimitIntegrationTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -27,6 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
+@SpringBootTest
 @AutoConfigureMockMvc
 public class RateLimitIntegrationTest extends AbstractIntegrationTest {
 
